811db2a5e613addbd18ad10942bb47b7638fadee
aaroln <lahtinenaaro77@gmail.com>
2023-11-21 22:03:25 +1100

    changes

--- app.py
+++ app.py
@@ -1,50 +1,17 @@
+# Imports
 import requests
-import pandas
-
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import pandas as pd
 
 app = Flask(__name__)
 
-# Load dataset
-dataOS = pd.read_csv('openSpace.csv')
+# Load Dataset
+dataPop = pd.read_csv('population.csv')
+dataOs = pd.read_csv('openSpace.csv')
 
-column_name = 'Value'
+@app.route("/")
+def countrySearch():
+    return ""
 
-# Check if the column exists in the DataFrame
-if column_name in dataOS.columns:
-    # Calculate the average of the specified column
-    average_value = dataOS[column_name].mean()
-    
-    print(f"The average value of '{column_name}' is: {average_value}")
-else:
-    print(f"Column '{column_name}' not found in the CSV file.")
-
-
-@app.route('/api/get_value', methods=['POST'])
-def get_value():
-    query = request.json.get("query")
-    country = dataOS[dataOS["GeoAreaName"].str.lower() == query.lower()]
-    if not cities.empty:
-        countryData = {}
-        
-        for index, row in country.iterrows():
-            city_name = row["Cities"]
-            city_value = row["Value"]
-            
-            # Add the city name and value to the dictionary
-            countryData[city_name] = city_value
-
-        result = {
-            "Country": query,
-            "Cities": countryData
-        }
-        return jsonify(result)
-    return jsonify({"ERROR": "Country not found"}), 404
- 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
