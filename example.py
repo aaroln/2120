49a7c2daf3fa74ec10e559eec96d2550ecb6ee54
aaroln <lahtinenaaro77@gmail.com>
2023-11-22 14:11:41 +1100

    big man changes

--- example.py
+++ example.py
@@ -1,16 +1,9 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 
-# Sample data
-x_values = [1, 2, 3, 4, 5]
-y_values = [10, 15, 7, 12, 9]
+# Example DataFrame with country names as the index
+data = {'Column1': [1, 2, 3], 'Column2': [4, 5, 6]}
+country_names = ['CountryA', 'CountryB', 'CountryC']
+df = pd.DataFrame(data, index=country_names)
 
-# Create a line plot
-plot = plt.plot(x_values, y_values, marker='o')
-
-# Add labels and title
-plt.xlabel('X-axis Label')
-plt.ylabel('Y-axis Label')
-plt.title('Simple Line Plot')
-
-# Display the plot
-plt.show()
+# Accessing the index
+print(df.index)
